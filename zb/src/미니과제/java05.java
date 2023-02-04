92bc9d21af5cb6f8fac28bee28b4a6a61e54a83a
IamJaehyun <lvet0330@gmail.com>
2023-02-04 12:16:24 +0900

    #9 오류 수정 및 주석처리
    3. 주석 추가 및 기본 입장료 10000원 미표기 수정
    4. Random.nextInt 부분 작성 오류로 수정
    5. 1일의 요일이 일요일일 때 한 줄 띄워지는 부분 수정
    6. 당선인 동률시 재투표 부분 수정 및 main 과 solution 분리

--- zb/src/미니과제/java05.java
+++ zb/src/미니과제/java05.java
@@ -3,6 +3,7 @@ package 미니과제;
 import java.time.LocalDate;
 import java.time.temporal.ChronoField;
 import java.util.Scanner;
+
 /*
     안재현
     달력 출력 프로그램
@@ -12,8 +13,8 @@ import java.util.Scanner;
     날짜는 LoaclDate클래스를 이용 (Calendar와 Date클래스도 이용 가능)
  */
 public class java05 {
-    public static void solution(int year, int month){
-        int cnt= 0;
+    public static void solution(int year, int month) {
+        int cnt = 0;
 
         System.out.printf("[%04d년%02d월]\n", year, month);
         System.out.println("일\t월\t화\t수\t목\t금\t토\t");
@@ -24,10 +25,13 @@ public class java05 {
         int lastDate = base.lengthOfMonth(); //마지막 날짜
 
 
-        for (int i = 0; i < firstDay; i++) {
-            cnt++;
+        //첫 날이 월요일인 날이 있을 수 있으니 (firstDay != 7) 일때만 /t로 시작점 위치를 조정
+        if (firstDay != 7) {
+            for (int i = 0; i < firstDay; i++) {
+                System.out.print("\t");
+                cnt++;
+            }
         }
-        System.out.print("\t");
 
         for (int j = 1; j <= lastDate; j++) {
             System.out.printf("%02d\t", j);
@@ -44,18 +48,18 @@ public class java05 {
 
         Scanner sc = new Scanner(System.in);
 
-        while (true){
+        while (true) {
             System.out.println("[달력 출력 프로그램]");
             System.out.print("달력의 년도를 입력해 주세요. (yyyy):");
             year = sc.nextInt();
-            if(year < 0) {
+            if (year < 0) {
                 System.out.println("년도 입력이 잘못되었습니다. 처음으로 돌아갑니다.\n");
                 continue;
             }
 
             System.out.print("달력의 월을 입력해 주세요. (mm):");
             month = sc.nextInt();
-            if(month <=0 || month > 12) {
+            if (month <= 0 || month > 12) {
                 System.out.println("월 입력이 잘못되었습니다. 처음으로 돌아갑니다.\n");
                 continue;
             }
