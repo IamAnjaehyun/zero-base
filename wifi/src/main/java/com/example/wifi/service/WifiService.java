e1f7c1a0305ae4c11ceb87b4de8f6f184e095394
IamJaehyun <lvet0330@gmail.com>
2023-03-22 13:33:56 +0900

    #39 service 기본 틀

--- wifi/src/main/java/com/example/wifi/service/WifiService.java
+++ wifi/src/main/java/com/example/wifi/service/WifiService.java
@@ -1,4 +1,35 @@
 package com.example.wifi.service;
 
+import com.example.wifi.dto.ResponseWifi;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WifiService {
+    public void saveWifi(){
+
+    }
+
+    public void deleteWifi(){
+
+    }
+
+    public void deleteAllWifi(){
+
+    }
+
+    public static double calculateDistance(Float x1, Float y1, Float x2, Float y2){
+        double x = Math.pow(Math.abs(x2 - x1), 2);
+        double y = Math.pow(Math.abs(y2 - y1), 2);
+
+        return Math.sqrt(x + y);
+    }
+
+    public List<ResponseWifi> showWifi(){
+        List<ResponseWifi> showWifi = new ArrayList<>();
+
+        return showWifi;
+    }
+
+
 }
