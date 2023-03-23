76075116aacc5f77ef2268eb55202966b423659d
IamJaehyun <lvet0330@gmail.com>
2023-03-23 21:11:21 +0900

    #42 request, response this 없는거 + 소문자->대문자 수정

--- wifi/src/main/java/com/example/wifi/dto/RequestWifi.java
+++ wifi/src/main/java/com/example/wifi/dto/RequestWifi.java
@@ -1,10 +1,9 @@
 package com.example.wifi.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.sql.Timestamp;
+@Data
 @Getter
 @Setter
 public class RequestWifi {
@@ -26,22 +25,23 @@ public class RequestWifi {
     Timestamp WORK_DTTM;        //"2023-03-20 10:58:16.0" 작업일자
 
     @Builder
-    public RequestWifi(String x_SWIFI_MGR_NO, String x_SWIFI_WRDOFC, String x_SWIFI_MAIN_NM, String x_SWIFI_ADRES1, String x_SWIFI_ADRES2, String x_SWIFI_INSTL_FLOOR, String x_SWIFI_INSTL_TY, String x_SWIFI_INSTL_MBY, String x_SWIFI_SVC_SE, String x_SWIFI_CMCWR, int x_SWIFI_CNSTC_YEAR, String x_SWIFI_INOUT_DOOR, String x_SWIFI_REMARS3, Float LAT, Float LNT, Timestamp WORK_DTTM) {
-        X_SWIFI_MGR_NO = x_SWIFI_MGR_NO;
-        X_SWIFI_WRDOFC = x_SWIFI_WRDOFC;
-        X_SWIFI_MAIN_NM = x_SWIFI_MAIN_NM;
-        X_SWIFI_ADRES1 = x_SWIFI_ADRES1;
-        X_SWIFI_ADRES2 = x_SWIFI_ADRES2;
-        X_SWIFI_INSTL_FLOOR = x_SWIFI_INSTL_FLOOR;
-        X_SWIFI_INSTL_TY = x_SWIFI_INSTL_TY;
-        X_SWIFI_INSTL_MBY = x_SWIFI_INSTL_MBY;
-        X_SWIFI_SVC_SE = x_SWIFI_SVC_SE;
-        X_SWIFI_CMCWR = x_SWIFI_CMCWR;
-        X_SWIFI_CNSTC_YEAR = x_SWIFI_CNSTC_YEAR;
-        X_SWIFI_INOUT_DOOR = x_SWIFI_INOUT_DOOR;
-        X_SWIFI_REMARS3 = x_SWIFI_REMARS3;
+    public RequestWifi(String X_SWIFI_MGR_NO, String X_SWIFI_WRDOFC, String X_SWIFI_MAIN_NM, String X_SWIFI_ADRES1, String X_SWIFI_ADRES2, String X_SWIFI_INSTL_FLOOR, String X_SWIFI_INSTL_TY, String X_SWIFI_INSTL_MBY, String X_SWIFI_SVC_SE, String X_SWIFI_CMCWR, int X_SWIFI_CNSTC_YEAR, String X_SWIFI_INOUT_DOOR, String X_SWIFI_REMARS3, Float LAT, Float LNT, Timestamp WORK_DTTM) {
+        this.X_SWIFI_MGR_NO = X_SWIFI_MGR_NO;
+        this.X_SWIFI_WRDOFC = X_SWIFI_WRDOFC;
+        this.X_SWIFI_MAIN_NM = X_SWIFI_MAIN_NM;
+        this.X_SWIFI_ADRES1 = X_SWIFI_ADRES1;
+        this.X_SWIFI_ADRES2 = X_SWIFI_ADRES2;
+        this.X_SWIFI_INSTL_FLOOR = X_SWIFI_INSTL_FLOOR;
+        this.X_SWIFI_INSTL_TY = X_SWIFI_INSTL_TY;
+        this.X_SWIFI_INSTL_MBY = X_SWIFI_INSTL_MBY;
+        this.X_SWIFI_SVC_SE = X_SWIFI_SVC_SE;
+        this.X_SWIFI_CMCWR = X_SWIFI_CMCWR;
+        this.X_SWIFI_CNSTC_YEAR = X_SWIFI_CNSTC_YEAR;
+        this.X_SWIFI_INOUT_DOOR = X_SWIFI_INOUT_DOOR;
+        this.X_SWIFI_REMARS3 = X_SWIFI_REMARS3;
         this.LAT = LAT;
         this.LNT = LNT;
         this.WORK_DTTM = WORK_DTTM;
     }
+
 }
