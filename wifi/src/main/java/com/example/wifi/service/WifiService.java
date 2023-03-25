5d6a8e8bee82c24fc37c6dd96d7fcc4f712194e5
IamJaehyun <lvet0330@gmail.com>
2023-03-25 22:39:55 +0900

    #50 add ERD & etc..

--- wifi/src/main/java/com/example/wifi/service/WifiService.java
+++ wifi/src/main/java/com/example/wifi/service/WifiService.java
@@ -20,7 +20,7 @@ public class WifiService {
             String url = "jdbc:sqlite:C:/sqllite/test.db";
             // 데이터베이스 연결
             conn = DriverManager.getConnection(url);
-            System.out.println("SQLite 데이터베이스에 연결되었습니다.");
+//            System.out.println("SQLite 데이터베이스에 연결되었습니다.");
 
             String sql = "insert into WIFI(X_SWIFI_MGR_NO, X_SWIFI_WRDOFC, X_SWIFI_MAIN_NM, X_SWIFI_ADRES1, X_SWIFI_ADRES2, X_SWIFI_INSTL_FLOOR, X_SWIFI_INSTL_TY, X_SWIFI_INSTL_MBY, X_SWIFI_SVC_SE, X_SWIFI_CMCWR, X_SWIFI_CNSTC_YEAR, X_SWIFI_INOUT_DOOR, X_SWIFI_REMARS3, lat, lnt, WORK_DTTM)" +
                     "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
