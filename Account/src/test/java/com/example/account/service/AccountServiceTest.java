3d2bb09b5259439ada6ae133a10e94d4be1a298d
Anjaehyun <lvet0330@gmail.com>
2023-04-19 02:09:16 +0900

    #146 verify, ArgumentCaptor, assertion "TEST"
    verify
    의존하고 있는 Mock이 해당되는 동작을 수행했는지 확인하는 검증
    
    ArgumentCaptor
    의존하고 있는 Mock에 전달된 데이터가 내가 의도하는 데이터가 맞는지 검증
    
    assertions
    다양한 단언(assertion) 방법들
    assertEquals("1234", captor.getValue().getAccountNumber());
    assertNotEquals("1234", captor.getValue().getAccountNumber());
    assertNull(result);
    assertNotNull(result);
    assertTrue(resut.getBoolean());
    assertFalse(resut.getBoolean());
    assertAll(
        () -> assertTrue(resut.getBoolean()),
        () -> assertFalse(resut.getBoolean())
    );

--- Account/src/test/java/com/example/account/service/AccountServiceTest.java
+++ Account/src/test/java/com/example/account/service/AccountServiceTest.java
@@ -1,20 +1,24 @@
 package com.example.account.service;
 
 import com.example.account.domain.Account;
-import com.example.account.domain.AccountStatus;
+import com.example.account.type.AccountStatus;
 import com.example.account.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
@@ -32,13 +36,30 @@ class AccountServiceTest {
                         .accountStatus(AccountStatus.UNREGISTERED)
                         .accountNumber("65789")
                         .build()));
+        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
         //when
         Account account = accountService.getAccount(4555L);
 
         //then
-        assertEquals("65789",account.getAccountNumber());
-        assertEquals(AccountStatus.UNREGISTERED,account.getAccountStatus());
+        verify(accountRepository, times(1)).findById(captor.capture()); //한 번 호출
+        verify(accountRepository, times(0)).save(any()); //저장하면 안되는 경우
+        assertEquals(45551L, captor.getValue());
+        assertNotEquals(45551L, captor.getValue());
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+    }
 
+    @Test
+    @DisplayName("계좌 조회 실패 - 음수로 조회")
+    void testFailedToSearchAccount() {
+        //given
+        //when
+
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> accountService.getAccount(-10L));//여기를 동작시키면 RuntimeException 발생 할 것이다
+
+        //then
+        assertEquals("Minus", exception.getMessage());
     }
 
 //    @Test
