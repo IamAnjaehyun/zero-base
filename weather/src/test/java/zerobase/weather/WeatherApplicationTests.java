3594bb0c40f7a92f8279cb440640d89a95401075
Anjaehyun <lvet0330@gmail.com>
2023-04-26 16:01:06 +0900

    #165 weather (junit - assertTest)
    assetSame(a,b) 객체 a,b가 같은 객체임을 확인, 객체 자체를 비교
    assertEquals(a,b) 객체 a,b가 일치함을 확인, 객체에 정의된 equals를 통해 비교
    assertArrayEquals 배열 a와 b가 일치함을 확인
    assertTrue(a) a가 참인지 확인
    assetNotNull(a) a가 null인지 확인

--- weather/src/test/java/zerobase/weather/WeatherApplicationTests.java
+++ weather/src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -3,11 +3,28 @@ package zerobase.weather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class WeatherApplicationTests {
+    //assetSame(a,b) 객체 a,b가 같은 객체임을 확인, 객체 자체를 비교
+    //assertEquals(a,b) 객체 a,b가 일치함을 확인, 객체에 정의된 equals를 통해 비교
+    //assertArrayEquals 배열 a와 b가 일치함을 확인
+    //assertTrue(a) a가 참인지 확인
+    //assetNotNull(a) a가 null인지 확인
+    @Test
+    void equalTest() {
+        assertEquals(1,1);
+    }
 
     @Test
-    void contextLoads() {
+    void nullTest() {
+        assertNull(null);
+    }
+
+    @Test
+    void trueTest() {
+        assertTrue(1==1);
     }
 
 }
