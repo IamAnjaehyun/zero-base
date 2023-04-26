c7ef640c6858953ce1c66cad684c0005091f424f
Anjaehyun <lvet0330@gmail.com>
2023-04-26 17:13:57 +0900

    #167 weather(Diary 구조 및 초기 세팅)

---
+++ weather/src/main/java/zerobase/service/DiaryService.java
@@ -0,0 +1,25 @@
+package zerobase.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class DiaryService {
+    @Value("{openweathermap.key}")
+    private String apiKey;
+
+    public void createDiary(LocalDate date, String text){
+        //open weather map 에서 데이터 받아오기
+        //받아온 날씨 데이터 파싱하기
+        //db에 저장하기
+        getWeatherString();
+    }
+
+    private String getWeatherString(){
+        String apiUrl = "https//api.openweathermap.org/data/2.5/weather?q=seoul&appid="+apiKey;
+        System.out.println(apiUrl);
+        return "";
+    }
+}
