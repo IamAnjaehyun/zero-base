b46d13e73bdc98e20e9b4bd2ec653fcce46368c2
Anjaehyun <lvet0330@gmail.com>
2023-04-28 15:21:34 +0900

    #170 weather (날씨 저장 스케쥴링)
    스케쥴링 적용 및 스케쥴링 적용에 따른 코드 리팩토링
    매일 새벽 1시에 자동으로 날씨 데이터를 api에서 받아와 db에 저장하도록 함
        @Transactional
    //    @Scheduled(cron = "0/5 * * * * *") //date가 pk라 여러번 해도 한 번만 동작함
        @Scheduled(cron = "0 0 1 * * *")

--- weather/src/main/java/zerobase/weather/WeatherApplication.java
+++ weather/src/main/java/zerobase/weather/WeatherApplication.java
@@ -2,10 +2,12 @@ package zerobase.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement //트랜잭션 매니지먼트 활성화
+@EnableScheduling //스케쥴링
 public class WeatherApplication {
 
     public static void main(String[] args) {
