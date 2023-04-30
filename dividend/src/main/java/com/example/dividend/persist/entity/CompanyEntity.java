97b9bf01da4b91848190fb50032cf483d7b950a5
Anjaehyun <lvet0330@gmail.com>
2023-04-30 22:59:38 +0900

    #182 dividend (entity 작성 및 repository 생성)

--- dividend/src/main/java/com/example/dividend/persist/entity/CompanyEntity.java
+++ dividend/src/main/java/com/example/dividend/persist/entity/CompanyEntity.java
@@ -1,4 +1,24 @@
 package com.example.dividend.persist.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "COMPANY")
+@Getter
+@ToString
+@NoArgsConstructor
 public class CompanyEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    private String ticker;
+
+    private String name;
 }
