be70a45c1f37bd15a30e86b58732c798bed9baad
Anjaehyun <lvet0330@gmail.com>
2023-05-02 21:07:45 +0900

    #189 dividend (생성자 클래스 생성 및, 실제 테스트위해 메인메서드 수정)

--- dividend/src/main/java/com/example/dividend/DividendApplication.java
+++ dividend/src/main/java/com/example/dividend/DividendApplication.java
@@ -1,17 +1,11 @@
 package com.example.dividend;
 
-import com.example.dividend.scrapper.Scrapper;
-import com.example.dividend.scrapper.YahooFinanceScraper;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DividendApplication {
     public static void main(String[] args) {
-//        SpringApplication.run(DividendApplication.class, args);
-
-        Scrapper scrapper = new YahooFinanceScraper(); //인터페이스 사용
-//        var result = scrapper.scrap(Company.builder().ticker("O").build());
-        var result = scrapper.scrapCompanyByTicker("MMM");
-        System.out.println(result);
+        SpringApplication.run(DividendApplication.class, args);
     }
 }
