5d561b65a80060d71b29c89d6c1c976ce000644d
Anjaehyun <lvet0330@gmail.com>
2023-05-02 21:07:29 +0900

    #188 dividend (company 명을 검색하여 찾을 수 있게 하기위한 클래스 생성)
    이름에 따라 검색 및 jpa 사용, addCompany 추가

--- dividend/src/main/java/com/example/dividend/persist/entity/CompanyEntity.java
+++ dividend/src/main/java/com/example/dividend/persist/entity/CompanyEntity.java
@@ -1,13 +1,11 @@
 package com.example.dividend.persist.entity;
 
+import com.example.dividend.model.Company;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity(name = "COMPANY")
 @Getter
@@ -15,10 +13,15 @@ import javax.persistence.Id;
 @NoArgsConstructor
 public class CompanyEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String ticker;
 
     private String name;
+
+    public CompanyEntity(Company company){
+        this.ticker = company.getTicker();
+        this.name = company.getName();
+    }
 }
