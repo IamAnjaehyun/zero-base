20b098f3e706f191a314358dd7f14824e576f644
Anjaehyun <lvet0330@gmail.com>
2023-05-04 19:16:47 +0900

    #195 dividend (스케쥴러 종료)
    //여러개의 쓰레드풀 사용하기 이해 SchedulerConfig를 사용하였으며, 
    yaml파일에 cron 발동 시간 및 기준을 작성하여 코드에서 수정하지 않아도 되도록 하였다.

--- dividend/src/main/java/com/example/dividend/scheduler/ScraperScheduler.java
+++ dividend/src/main/java/com/example/dividend/scheduler/ScraperScheduler.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -28,8 +29,19 @@ public class ScraperScheduler {
     private final DividendRepository dividendRepository;
     private final Scraper yahooFinanceScraper;
 
+    @Scheduled(fixedDelay = 1000)
+    public void test1() throws InterruptedException{
+        Thread.sleep(10000); //10초간 일시 정지
+        System.out.println(Thread.currentThread().getName() + " 테스트 1 : " + LocalDateTime.now());
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    public void test2() throws InterruptedException{
+        System.out.println(Thread.currentThread().getName() + " 테스트 2 : " + LocalDateTime.now());
+    }
+
     //일정 주기마다 반복 시분일월요일 순서
-    @Scheduled(cron = "0 0 0 * * *")
+//    @Scheduled(cron = "${scheduler.scrap.yahoo}")
     public void yahooFinanceScheduling() {
         log.info("scraping scheduler is started");
         //저장된 회사 목록을 조회
@@ -61,6 +73,5 @@ public class ScraperScheduler {
                 Thread.currentThread().interrupt();
             }
         }
-
     }
 }
