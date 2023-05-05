70d6d6959ae3550cb52842c7f12ff8e4a704124b
Anjaehyun <lvet0330@gmail.com>
2023-05-05 23:40:08 +0900

    #201 dividend (Jwt)
    잘못된 의존성 수정 
    
    echo 'jaehyun-spring-boot-dividend' | base64
    amFlaHl1bi1zcHJpbmctYm9vdC1kaXZpZGVuZAo=
    를 통해 jwt 암호키 생성
    
    토큰 생성, 파싱, 검증 구현

--- dividend/src/main/java/com/example/dividend/sercurity/TokenProvider.java
+++ dividend/src/main/java/com/example/dividend/sercurity/TokenProvider.java
@@ -1,4 +1,68 @@
 package com.example.dividend.sercurity;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class TokenProvider {
+
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; //1시간(1000 * 60 * 60)
+    private static final String KEY_ROLES = "roles";
+
+    @Value("{spring.jwt.secret}")
+    private String secretKey;
+
+    /**
+     * 토큰 생성(발급)
+     *
+     * @param username
+     * @param roles
+     * @return
+     */
+    private String generateToken(String username, List<String> roles) { //토큰 생성
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES, roles);
+
+
+        var now = new Date();
+        var expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME); //얼마나 토큰을 유효하게 할건지 (1시간)
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now) //토근 생성 시간
+                .setExpiration(expiredDate) //토큰 만료 시간
+                .signWith(SignatureAlgorithm.ES512, this.secretKey) //사용할 암호화 알고리즘, 비밀키
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return this.parseClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        if (!StringUtils.hasText(token)) return false;
+
+        var claims = this.parseClaims(token);
+        return !claims.getExpiration().before(new Date()); //토큰 이 유효한지 (만료시간이 현재보다 이전인지 아닌지)
+    }
+
+    private Claims parseClaims(String token) { //토큰 파싱
+        try {
+            return Jwts.parser().setSigningKey(this.secretKey).parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            //TODO
+            return e.getClaims();
+        }
+    }
 }
