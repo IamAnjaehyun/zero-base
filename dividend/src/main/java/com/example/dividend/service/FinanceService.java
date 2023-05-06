66df48c64e87423022acfd6d7e9d0bbfbb5ac2a0
Anjaehyun <lvet0330@gmail.com>
2023-05-06 21:29:22 +0900

    #205 dividend (예외 처리)
    Custom Error 정의 및 적용

--- dividend/src/main/java/com/example/dividend/service/FinanceService.java
+++ dividend/src/main/java/com/example/dividend/service/FinanceService.java
@@ -1,5 +1,6 @@
 package com.example.dividend.service;
 
+import com.example.dividend.exception.impl.NoCompanyException;
 import com.example.dividend.model.Company;
 import com.example.dividend.model.Dividend;
 import com.example.dividend.model.ScrapedResult;
@@ -31,7 +32,7 @@ public class FinanceService {
 
         //1. 회사명을 기준으로 회사 정보를 조회
         CompanyEntity company = this.companyRepository.findByName(companyName)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 회사명입니다.")); //에러뜨면 에러 넘김(orelsethrow)
+                .orElseThrow(() -> new NoCompanyException()); //에러뜨면 에러 넘김(orelsethrow)
 
         //2. 조회된 회사의 아이디로 배당금 정보 조회
         List<DividendEntity> dividendEntities = this.dividendRepository.findAllByCompanyId(company.getId());
