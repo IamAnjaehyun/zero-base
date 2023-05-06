5b2b937c295cfd373408a5b646e26ca53a63d27b
Anjaehyun <lvet0330@gmail.com>
2023-05-06 14:56:38 +0900

    #203 dividend (인증 기능 구현)
    회원가입, 로그인 인증 구현
    
    암호화방식 오류떠서 ES512 -> HS512로 변경하였더니 에러 해결
    
    단순한 로그인도 토큰 사용해서 하려고하니 굉장히 어렵다고 생각...

--- dividend/src/main/java/com/example/dividend/sercurity/TokenProvider.java
+++ dividend/src/main/java/com/example/dividend/sercurity/TokenProvider.java
@@ -1,5 +1,6 @@
 package com.example.dividend.sercurity;
 
+import com.example.dividend.service.MemberService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -7,6 +8,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -19,6 +23,8 @@ public class TokenProvider {
 
     private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; //1시간(1000 * 60 * 60)
     private static final String KEY_ROLES = "roles";
+
+    private final MemberService memberService;
 
     @Value("{spring.jwt.secret}")
     private String secretKey;
@@ -42,8 +48,13 @@ public class TokenProvider {
                 .setClaims(claims)
                 .setIssuedAt(now) //토근 생성 시간
                 .setExpiration(expiredDate) //토큰 만료 시간
-                .signWith(SignatureAlgorithm.ES512, this.secretKey) //사용할 암호화 알고리즘, 비밀키
+                .signWith(SignatureAlgorithm.HS512, this.secretKey) //사용할 암호화 알고리즘, 비밀키
                 .compact();
+    }
+
+    public Authentication getAuthentication(String jwt) {//인증정보 가져옴
+        UserDetails userDetails = this.memberService.loadUserByUsername(this.getUsername(jwt));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
     public String getUsername(String token) {
