7a0b12d9faff1c36592a181dec95ecc298490619
Anjaehyun <lvet0330@gmail.com>
2023-05-10 01:32:58 +0900

    #234 fintech (nginx 직접 작성하고 띄움)
    docker build -t nginxtest:2.0 .
    
    docker run nginxtest:2.0

--- fintech/css/src/main/kotlin/com/zerobase/css/controller/LoanReceiveController.kt
+++ fintech/css/src/main/kotlin/com/zerobase/css/controller/LoanReceiveController.kt
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+
+//hostname/css
 @RestController
 @RequestMapping("/css/api/v1")
 class LoanReceiveController(
