7a6e7035be151b5870f15afd48b7ddb4106e3e3e
Anjaehyun <lvet0330@gmail.com>
2023-05-14 16:11:12 +0900

    #258 cms (이메일 보내기 위한 코드 & 테스트코드)
    이메일 전송을 위한 코드..
    강사님께서 문제해결 능력을 늘려주신다..!

---
+++ cms/user-api/src/main/java/com/zerobase/cms/user/config/FeignConfig.java
@@ -0,0 +1,18 @@
+package com.zerobase.cms.user.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+
+@Configuration
+public class FeignConfig {
+
+    @Qualifier(value = "mailgun")
+    @Bean
+    public BasicAuthenticationInterceptor basicAuthenticationInterceptor(){
+        //TODO cd~~ 수정
+        return new BasicAuthenticationInterceptor("api","cd~~");
+    }
+
+}
