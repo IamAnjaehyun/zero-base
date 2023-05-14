6260a7c2b23e423f33a6abad4f3180eb8a9f3d83
Anjaehyun <lvet0330@gmail.com>
2023-05-14 23:48:34 +0900

    #259 cms (이메일 전송 테스트를 위한 클래스 주석처리 및 삭제)
    더이상 필요없는 클래스 및 파일 삭제~

--- cms/user-api/src/test/java/com/zerobase/cms/user/client/service/EmailSendServiceTest.java
+++ cms/user-api/src/test/java/com/zerobase/cms/user/client/service/EmailSendServiceTest.java
@@ -1,18 +1,17 @@
-package com.zerobase.cms.user.client.service;
-
-import com.zerobase.cms.user.service.test.EmailSendService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EmailSendServiceTest {
-    @Autowired
-    private EmailSendService emailSendService;
-
-    @Test
-    public void EmailTest(){
-        String response= emailSendService.sendEmail();
-        System.out.println(response);
-    }
-}
+//package com.zerobase.cms.user.client.service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EmailSendServiceTest {
+//    @Autowired
+//    private EmailSendService emailSendService;
+//
+//    @Test
+//    public void EmailTest(){
+//        String response= emailSendService.sendEmail();
+//        System.out.println(response);
+//    }
+//}
