45d34a4d9c13fc6dcff28fc55f1ebd578d6a131e
Anjaehyun <lvet0330@gmail.com>
2023-05-14 23:59:18 +0900

    #261 cms (for signUpTest)
    Test에서는 properties 설정값이 다름!
    
    강사님께선 브랜치를 새로 따서 커밋하는걸 보여주셔서 브랜치를 새로 따는 법과 관리하는 법을 알게 되었지만,
    본인은 한 레포에 여러 프로젝트들이 있기 때문에.. 메인 브랜치 유지..!

---
+++ cms/user-api/src/test/java/com/zerobase/cms/user/service/SignUpCustomerServiceTest.java
@@ -0,0 +1,38 @@
+package com.zerobase.cms.user.service;
+
+import com.zerobase.cms.user.domain.SignUpForm;
+import com.zerobase.cms.user.domain.model.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class SignUpCustomerServiceTest {
+
+    @Autowired
+    private SignUpCustomerService service;
+
+    @Test
+    void signUp() {
+        SignUpForm form = SignUpForm.builder()
+                .name("name")
+                .birth(LocalDate.now())
+                .email("abc@gmail.com")
+                .password("1")
+                .phone("0111111111")
+                .build();
+
+
+        Customer c  = service.signUp(form);
+        //null이 안뜨면 회원가입 성공
+//        Assert.isTrue(service.signUp(form).getId() != null);
+        assertNotNull(c.getId());
+        assertNotNull(c.getCreatedAt());
+
+
+    }
+}
